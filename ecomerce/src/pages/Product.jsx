46ce5415af3467dc4314fe063b922bca547a1b51
Gabriel Cazar <gabriel_cazar@hotmail.com>
2023-01-22 00:53:40 -0500

    producto

--- ecomerce/src/pages/Product.jsx
+++ ecomerce/src/pages/Product.jsx
@@ -1,98 +1,119 @@
+import { useForm } from "@/hooks/useForm";
+import useGetCategorias from "@/hooks/useGetCategorias";
+import useGetPresentaciones from "@/hooks/useGetPresentaciones";
+import { useProducto } from "@/hooks/useProducto";
 import React from "react";
 
 const Product = () => {
+  const { getProductoActivo } = useProducto();
+  const producto = getProductoActivo();
+  const { isLoadingCategorias, getStateCategorias } = useGetCategorias();
+  const { isLoadingPresentaciones, getStatePresentaciones } =
+    useGetPresentaciones();
+  const { values, handleInputChange, reset, handleInputCheck } = useForm({
+    IdProducto: producto ? producto.idProducto : 0,
+    NombreProducto: producto ? producto.nombreProducto : "",
+    DescripcionProducto: producto ? producto.descripcionProducto : "",
+    IdCategoria: producto ? producto.idCategoria : 0,
+    IdPresentacion: producto ? producto.idProducto : 0,
+    Cantidad_Producto: producto ? producto.idProducto : 0,
+    Costo_unitario: producto ? producto.idProducto : 0,
+    GrabaIva: producto ? producto.idProducto : 0,
+    PorcentajeMargenGanancia: producto ? producto.idProducto : 0,
+    ProductoActivo: producto ? producto.idProducto : 0,
+  });
+  const {
+    IdProducto,
+    NombreProducto,
+    DescripcionProducto,
+    IdCategoria,
+    IdPresentacion,
+    Cantidad_Producto,
+    Costo_unitario,
+    GrabaIva,
+    PorcentajeMargenGanancia,
+    ProductoActivo,
+  } = values;
+  if (isLoadingCategorias && isLoadingPresentaciones)
+    return <h2 className="title is-2">Cargando </h2>;
+  const listCategorias = getStateCategorias();
+  const listPresentacion = getStatePresentaciones();
+
   return (
     <>
-      <div className="field">
-        <label className="label">Name</label>
-        <div className="control">
-          <input className="input" type="text" placeholder="Text input" />
-        </div>
-      </div>
-
-      <div className="field">
-        <label className="label">Username</label>
-        <div className="control has-icons-left has-icons-right">
-          <input
-            className="input is-success"
-            type="text"
-            placeholder="Text input"
-          />
-          <span className="icon is-small is-left">
-            <i className="fas fa-user"></i>
-          </span>
-          <span className="icon is-small is-right">
-            <i className="fas fa-check"></i>
-          </span>
-        </div>
-        <p className="help is-success">This username is available</p>
-      </div>
-
-      <div className="field">
-        <label className="label">Email</label>
-        <div className="control has-icons-left has-icons-right">
-          <input
-            className="input is-danger"
-            type="email"
-            placeholder="Email input"
-          />
-          <span className="icon is-small is-left">
-            <i className="fas fa-envelope"></i>
-          </span>
-          <span className="icon is-small is-right">
-            <i className="fas fa-exclamation-triangle"></i>
-          </span>
-        </div>
-        <p className="help is-danger">This email is invalid</p>
-      </div>
-
-      <div className="field">
-        <label className="label">Subject</label>
-        <div className="control">
-          <div className="select">
-            <select>
-              <option>Select dropdown</option>
-              <option>With options</option>
-            </select>
+      <div className="container">
+        <h2 className="title is-2">Agregar Producto </h2>
+        <div className="field">
+          <label className="label">Nombre Producto</label>
+          <div className="control">
+            <input
+              className="input"
+              name="NombreProducto"
+              value={NombreProducto}
+              type="text"
+              placeholder="Nombre Producto"
+              onChange={handleInputChange}
+            />
           </div>
         </div>
-      </div>
 
-      <div className="field">
-        <label className="label">Message</label>
-        <div className="control">
-          <textarea className="textarea" placeholder="Textarea"></textarea>
+        <div className="field">
+          <label className="label">Descripcion Producto</label>
+          <div className="control has-icons-left has-icons-right">
+            <textarea
+              className="textarea is-success"
+              type="text"
+              placeholder="Text input"
+              name="DescripcionProducto"
+              values={DescripcionProducto}
+              onChange={handleInputChange}
+            />
+          </div>
         </div>
-      </div>
 
-      <div className="field">
-        <div className="control">
-          <label className="checkbox">
-            <input type="checkbox" />I agree to the{" "}
-            <a href="#">terms and conditions</a>
-          </label>
-        </div>
-      </div>
+        <div className="field">
+          <label className="label">Subject</label>
+          <div className="control">
+            <div className="select">
+              <select>
+                {listCategorias.map(() => {})}
 
-      <div className="field">
-        <div className="control">
-          <label className="radio">
-            <input type="radio" name="question" />
-            Yes
-          </label>
-          <label className="radio">
-            <input type="radio" name="question" />
-            No
-          </label>
+                <option>Select dropdown</option>
+                <option>With options</option>
+              </select>
+            </div>
+          </div>
         </div>
-      </div>
 
-      <div className="field is-grouped">
-        <div className="control">
-          <button className="button is-link">Submit</button>
+        <div className="field">
+          <div className="control">
+            <label className="checkbox">
+              <input type="checkbox" />I agree to the{" "}
+              <a href="#">terms and conditions</a>
+            </label>
+          </div>
         </div>
-        <div className="control">
-          <button className="button is-link is-light">Cancel</button>
+
+        <div className="field">
+          <div className="control">
+            <label className="radio">
+              <input type="radio" name="question" />
+              Yes
+            </label>
+            <label className="radio">
+              <input type="radio" name="question" />
+              No
+            </label>
+          </div>
+        </div>
+
+        <div className="field is-grouped">
+          <div className="control">
+            <button className="button is-link">Submit</button>
+          </div>
+          <div className="control">
+            <button className="button is-link is-light">Cancel</button>
+          </div>
         </div>
       </div>
     </>
